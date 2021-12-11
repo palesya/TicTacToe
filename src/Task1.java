2e8cdfc1c7cb70a79dde8a60f5856954dcfe0d89
”Alesya <palesyav89@gmail.com>
2021-12-11 16:16:22 +0300

    Initial commit

---
+++ src/Task1.java
@@ -0,0 +1,131 @@
+//Задача 1
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task1 {
+
+    private static Random random = new Random();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите размерность матрицы: ");
+        int n = scanner.nextInt();
+        int[][] generatedArray = generateArray(n);
+        printGeneratedArray(generatedArray, n);
+
+        int sumOfEvenDiagonalElements = sumMajorDiagonal(generatedArray, n);
+        System.out.println("Сумма чётных элементов главной диагонали: " + sumOfEvenDiagonalElements);
+        System.out.println("Нечетные элементы находящиеся под главной диагональю(включительно):");
+        fintNotEvenElements(generatedArray, n);
+        int multiMajorDiagonal = multiplyMajorDiagonal(generatedArray, n);
+        int multiSideDiagonal = multiplySideDiagonal(generatedArray, n);
+        System.out.println();
+        if (multiMajorDiagonal > multiSideDiagonal) {
+            System.out.println("Произведение элементов главной диагонали больше и равно: " + multiMajorDiagonal);
+        } else if (multiMajorDiagonal < multiSideDiagonal) {
+            System.out.println("Произведение элементов побочной диагонали больше и равно: " + multiSideDiagonal);
+        } else {
+            System.out.println("Произведение элементов обеих диагоналей равно: " + multiMajorDiagonal);
+        }
+        int sumOfEvenElements = sumEvenElements(generatedArray, n);
+        if (sumOfEvenElements == -1) {
+            System.out.println("Четных элементов над побочной диагональю нет.");
+        } else {
+            System.out.println("Cумма четных элементов стоящих над побочной диагональю (не включительно): " + sumOfEvenElements);
+        }
+        int[][] transposedArray = transposeArray(generatedArray, n);
+        System.out.println("Транспонированная матрица: ");
+        printGeneratedArray(transposedArray, n);
+
+    }
+
+    private static int[][] generateArray(int n) {
+        int[][] array = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                array[i][j] = random.nextInt(51);
+            }
+        }
+        return array;
+    }
+
+    private static void printGeneratedArray(int[][] array, int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(" " + array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static int sumMajorDiagonal(int[][] array, int n) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (array[i][i] % 2 == 0)
+                sum += array[i][i];
+        }
+        return sum;
+    }
+
+    private static void fintNotEvenElements(int[][] array, int n) {
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (array[i][j] % 2 != 0) {
+                    System.out.print(array[i][j] + " ");
+                    count++;
+                }
+            }
+        }
+    }
+
+    private static int multiplyMajorDiagonal(int[][] array, int n) {
+        int multi = array[0][0];
+        for (int i = 1; i < n; i++) {
+            multi *= array[i][i];
+        }
+        return multi;
+    }
+
+    private static int multiplySideDiagonal(int[][] array, int n) {
+        int maxIndex = n - 1;
+        int multi = array[maxIndex][0];
+        for (int i = maxIndex - 1; i >= 0; i--) {
+            multi *= array[i][maxIndex - i];
+        }
+        return multi;
+    }
+
+    private static int sumEvenElements(int[][] array, int n) {
+        int sum = -1;
+        int sum1 = 0;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (array[i][j] % 2 == 0) {
+                    sum1 += array[i][j];
+                    count++;
+                }
+            }
+        }
+        if (count > 0) {
+            sum = sum1;
+        }
+        return sum;
+    }
+
+    private static int[][] transposeArray(int[][] array, int n) {
+        int temp;
+       for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                temp = array[i][j];
+                array[i][j] = array[j][i];
+                array[j][i] = temp;
+            }
+        }
+        return array;
+    }
+
+}
+
---
+++ src/TicTacToe.java
@@ -0,0 +1,148 @@
+//Крестики-нолики
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TicTacToe {
+
+    public static void main(String[] args) {
+        String[][] generatedArray = generateArray();
+        //пустой
+        printGeneratedArray(generatedArray);
+        //1-ый ход
+        String[][] generatedArrayWithX = generateArrayWithX(generatedArray);
+        printGeneratedArray(generatedArrayWithX);
+        //2-ой ход
+        String[][] generatedArrayWith0 = generateArrayWith0(generatedArrayWithX);
+        printGeneratedArray(generatedArrayWith0);
+        //3-ий ход
+        printGeneratedArray(generateArrayWithX(generatedArrayWith0));
+        //4-ый ход
+        printGeneratedArray(generateArrayWith0(generatedArrayWithX));
+        //5-ый ход
+        printGeneratedArray(generateArrayWithX(generatedArrayWith0));
+        if (!findIfWin(generatedArrayWith0)) {
+            //6-ой ход
+            printGeneratedArray(generateArrayWith0(generatedArrayWithX));
+            if (!findIfWin(generatedArrayWithX)) {
+                //7-ой ход
+                printGeneratedArray(generateArrayWithX(generatedArrayWith0));
+                if (!findIfWin(generatedArrayWith0)) {
+                    //8-ой ход
+                    printGeneratedArray(generateArrayWith0(generatedArrayWithX));
+                    if (!findIfWin(generatedArrayWithX)) {
+                        //9-ый ход
+                        printGeneratedArray(generateArrayWithX(generatedArrayWith0));
+                        if (!findIfWin(generatedArrayWith0)) {
+                            System.out.println("Игра окончена с результатом ничья!");
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("Игра окончена. Есть победитель!");
+    }
+
+
+    private static String[][] generateArray() {
+        String[][] array = new String[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                array[i][j] = "_";
+            }
+        }
+        return array;
+    }
+
+    private static void printGeneratedArray(String[][] array) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(" " + array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    //проверить ячейку на пустоту и сделать ход для X
+    private static String[][] generateArrayWithX(String[][] array) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите номер заполняемой ячейки по горизонтали (от 1 до 3): ");
+        int x = scanner.nextInt();
+        System.out.println("Введите номер заполняемой ячейки по вертикали (от 1 до 3): ");
+        int y = scanner.nextInt();
+        if (x < 1 || y < 1 || x > 3 || y > 3) {
+            System.out.println("Неверное число. Номер ячейки должен быть от 1 до 3!");
+        } else {
+            if (array[x - 1][y - 1] == "_") {
+                array[x - 1][y - 1] = "X";
+
+            } else {
+                System.out.println("Данная ячейка занята. Сделайте другой ход.");
+
+            }
+        }
+
+        return array;
+    }
+
+    //проверить ячейку на пустоту и сделать ход для 0
+    private static String[][] generateArrayWith0(String[][] array) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите номер заполняемой ячейки по горизонтали (от 1 до 3): ");
+        int x = scanner.nextInt();
+        System.out.println("Введите номер заполняемой ячейки по вертикали (от 1 до 3): ");
+        int y = scanner.nextInt();
+        if (x < 1 || y < 1 || x > 3 || y > 3) {
+            System.out.println("Неверное число. Номер ячейки должен быть от 1 до 3!");
+        } else {
+            if (array[x - 1][y - 1] == "_") {
+                array[x - 1][y - 1] = "0";
+            } else {
+                System.out.println("Данная ячейка занята. Сделайте другой ход.");
+            }
+        }
+        return array;
+    }
+
+    //Проверить наличие победы
+    private static boolean findIfWin(String[][] array) {
+        boolean ifWin;
+        String[] line1 = new String[3];
+        String[] line2 = new String[3];
+        String[] line3 = new String[3];
+        String[] column1 = new String[3];
+        String[] column2 = new String[3];
+        String[] column3 = new String[3];
+        String[] mainDiagonal = new String[3];
+        String[] SideDiagonal = new String[3];
+        String[] WinX = {"X", "X", "X"};
+        String[] Win0 = {"0", "0", "0"};
+        for (int i = 0; i < 3; i++) {
+            line1[i] = array[0][i];
+            line2[i] = array[1][i];
+            line3[i] = array[2][i];
+            column1[i] = array[i][0];
+            column2[i] = array[i][1];
+            column3[i] = array[i][2];
+            SideDiagonal[i] = array[2 - i][i];
+            mainDiagonal[i] = array[i][i];
+        }
+        ifWin = Arrays.equals(line1, WinX) | Arrays.equals(line1, Win0) |
+                Arrays.equals(line2, WinX) | Arrays.equals(line2, Win0) |
+                Arrays.equals(line3, WinX) | Arrays.equals(line3, Win0) |
+                Arrays.equals(column1, WinX) | Arrays.equals(column1, Win0) |
+                Arrays.equals(column2, WinX) | Arrays.equals(column2, Win0) |
+                Arrays.equals(column3, WinX) | Arrays.equals(column3, Win0) |
+                Arrays.equals(mainDiagonal, WinX) | Arrays.equals(mainDiagonal, Win0) |
+                Arrays.equals(SideDiagonal, WinX) | Arrays.equals(SideDiagonal, Win0);
+
+        return ifWin;
+    }
+
+
+}
+
+
+
+
+
